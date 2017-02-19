ea2e0bda4360bda705c4f2c127b3d7f849bd3e9b
slavas <slavasit@gmail.com>
2017-02-19 17:33:29 +0200

    new

---
+++ javascript train.js
@@ -0,0 +1,8 @@
+var animals = [
+    { name: 'Fluffykins', species: 'rabbit' },
+    { name: 'Caro',       species: 'dog' },
+    { name: 'Hamilton',   species: 'dog' },
+    { name: 'Harold',     species: 'fish' },
+    { name: 'Ursula',     species: 'cat' },
+    { name: 'Jimmy',      species: 'fish' }
+]
