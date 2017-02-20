e4b9d46f21b1cdf94b1d620c49ab11053fec5342
slavas <slavasit@gmail.com>
2017-02-20 11:29:09 +0200

    new

--- javascript practice.js
+++ javascript practice.js
@@ -1,15 +1,1 @@
 
-// map functions
-
-
-var animals = [
-    { name: 'Fluffykins', species: 'rabbit' },
-    { name: 'Caro',       species: 'dog' },
-    { name: 'Hamilton',   species: 'dog' },
-    { name: 'Harold',     species: 'fish' },
-    { name: 'Ursula',     species: 'cat' },
-    { name: 'Jimmy',      species: 'fish' }
-]
-
-var names = animals.map((x) => x.name)
-console.log(names);
